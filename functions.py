a672f49c48a20451611c49b29e56425790cc1857
Joffrey <joffreybvn@gmail.com>
2020-12-09 12:30:49 +0100

    branch cleaned

--- functions.py
+++ functions.py
@@ -1,9 +1,11 @@
 from models.AddressModel import *
 from models.PersonModel import *
 
+
 def generate_address():
     address = Address()
     return address.datas()
+
 
 def generate_person(b_female):
     person = Person(b_female, generate_address())
