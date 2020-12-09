ce16c21c3f6b3cd9f52631c7ec98c10a9539d69a
leersmathieu <leers.mathieu@gmail.com>
2020-12-09 13:06:16 +0100

    epic advancement

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,41 @@
 // main js
+
+const selection_menu = document.querySelector('li.selection');
+const general_menu = document.querySelector('li.general');
+const specialization_menu = document.querySelector('li.specialization');
+
+const selection = document.querySelector('#selection');
+const general = document.querySelector('#general');
+const specialization = document.querySelector('#specialization');
+
+const scrollElement = window.document.scrollingElement || window.document.body || window.document.documentElement;
+
+selection_menu.addEventListener('click', () => {
+    // use anime.js
+    anime({
+        targets: scrollElement,
+        scrollTop: 0,
+        duration: 500,
+        easing: 'easeInOutQuad'
+    });
+});
+
+general_menu.addEventListener('click', () => {
+    // use anime.js
+    anime({
+        targets: scrollElement,
+        scrollTop:general.offsetTop - 60,
+        duration: 500,
+        easing: 'easeInOutQuad'
+    });
+});
+
+specialization_menu.addEventListener('click', () => {
+    // use anime.js
+    anime({
+        targets: scrollElement,
+        scrollTop:specialization.offsetTop - 60,
+        duration: 500,
+        easing: 'easeInOutQuad'
+    });
+});
